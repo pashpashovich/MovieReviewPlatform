6f3c590aa6690e4565589663698eb23af698748a
pashpashovich <pavel.kosovich2004@gmail.com>
2024-11-30 20:55:20 +0300

    Added docker-compose and hibernate config

---
+++ src/main/java/by/innowise/moviereview/util/HibernateUtil.java
@@ -0,0 +1,35 @@
+package by.innowise.moviereview.util;
+
+import lombok.Getter;
+import lombok.experimental.UtilityClass;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+@UtilityClass
+public class HibernateUtil {
+
+    @Getter
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+//                    .addAnnotatedClass(Post.class)
+
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+
+}
